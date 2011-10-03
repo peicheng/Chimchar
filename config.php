31a14a17ba2c15344471f667a033e949730c27f5
hbc <bcxxxxxx@gmail.com>
2011-10-03 09:30:03 +0800

    basic configure file

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct access allowed!');
+
+/* Here is some basic setting... */
+
+// Plugins
+define('PLUGINS', realpath(BASEPATH.'/plugins').'/');
+
+// Template
+define('TPL', realpath(BASEPATH.'/tpl').'/');
+
+// Logfile
+define('LOGFILE', realpath(BASEPATH.'log.txt'));
+
+// Database
+define('Database', 'sqlite:chimchar.db');
+
+// You can change the timezone here.
+date_default_timezone_set('Asia/Shanghai');
+define('DATE_FORMAT', 'Y-m-d h:i:s');
+?>
